e691c6a0dd761c28f3f720be9ec982e6afe7e980
Jesus Puentes <jopt98@gmail.com>
2025-12-22 20:35:31 -0600

    Commented non important projects

--- src/assets/constants/projects.ts
+++ src/assets/constants/projects.ts
@@ -37,70 +37,70 @@ export const PROJECTS = {
             "createdAt": "2025-01-12T14:12:06.425+00:00",
             "updatedAt": "2025-01-12T14:12:06.425+00:00"
         },
-        {
-            "id": 52,
-            "projectName": "RPS Backend",
-            "projectDescription": "Backend made for the RPS project, with JWT's and express",
-            "projectTechnologies": [
-                {
-                    "id": 6,
-                    "techName": "bx bxl-nodejs",
-                    "state": true,
-                    "createdAt": "2025-01-10T23:44:43.716+00:00",
-                    "updatedAt": "2025-01-10T23:44:43.716+00:00"
-                },
-                {
-                    "id": 7,
-                    "techName": "bx bxl-mongodb",
-                    "state": true,
-                    "createdAt": "2025-01-10T23:44:47.976+00:00",
-                    "updatedAt": "2025-01-10T23:44:47.976+00:00"
-                }
-            ],
-            "projectTopic": "WEB",
-            "projectState": true,
-            "projectUrl": "https://github.com/Jopt05/RPSBackend",
-            "createdAt": "2025-01-12T23:08:55.026+00:00",
-            "updatedAt": "2025-01-12T23:08:55.026+00:00"
-        },
-        {
-            "id": 53,
-            "projectName": "My Unsplash",
-            "projectDescription": "Small challenge in which file uploading and accounts are used.",
-            "projectTechnologies": [
-                {
-                    "id": 1,
-                    "techName": "bx bxl-react",
-                    "state": true,
-                    "createdAt": "2025-01-10T23:43:55.380+00:00",
-                    "updatedAt": "2025-01-10T23:43:55.380+00:00"
-                }
-            ],
-            "projectTopic": "DESKTOP",
-            "projectState": true,
-            "projectUrl": "https://my-unsplash-frontend-five.vercel.app/",
-            "createdAt": "2025-01-12T23:09:53.002+00:00",
-            "updatedAt": "2025-01-12T23:09:53.002+00:00"
-        },
-        {
-            "id": 54,
-            "projectName": "Notes App",
-            "projectDescription": "Project made with Expo React Native",
-            "projectTechnologies": [
-                {
-                    "id": 9,
-                    "techName": "bx bx-mobile-alt",
-                    "state": true,
-                    "createdAt": "2025-01-10T23:44:57.496+00:00",
-                    "updatedAt": "2025-01-10T23:44:57.496+00:00"
-                }
-            ],
-            "projectTopic": "MOBILE",
-            "projectState": true,
-            "projectUrl": "https://expo.dev/accounts/jopt05/projects/react-native-notes-app/builds/55f4bc9a-cd7c-424e-85bc-58d3b6716b4d",
-            "createdAt": "2025-01-12T23:10:55.296+00:00",
-            "updatedAt": "2025-01-13T02:17:29.755+00:00"
-        },
+        // {
+        //     "id": 52,
+        //     "projectName": "RPS Backend",
+        //     "projectDescription": "Backend made for the RPS project, with JWT's and express",
+        //     "projectTechnologies": [
+        //         {
+        //             "id": 6,
+        //             "techName": "bx bxl-nodejs",
+        //             "state": true,
+        //             "createdAt": "2025-01-10T23:44:43.716+00:00",
+        //             "updatedAt": "2025-01-10T23:44:43.716+00:00"
+        //         },
+        //         {
+        //             "id": 7,
+        //             "techName": "bx bxl-mongodb",
+        //             "state": true,
+        //             "createdAt": "2025-01-10T23:44:47.976+00:00",
+        //             "updatedAt": "2025-01-10T23:44:47.976+00:00"
+        //         }
+        //     ],
+        //     "projectTopic": "WEB",
+        //     "projectState": true,
+        //     "projectUrl": "https://github.com/Jopt05/RPSBackend",
+        //     "createdAt": "2025-01-12T23:08:55.026+00:00",
+        //     "updatedAt": "2025-01-12T23:08:55.026+00:00"
+        // },
+        // {
+        //     "id": 53,
+        //     "projectName": "My Unsplash",
+        //     "projectDescription": "Small challenge in which file uploading and accounts are used.",
+        //     "projectTechnologies": [
+        //         {
+        //             "id": 1,
+        //             "techName": "bx bxl-react",
+        //             "state": true,
+        //             "createdAt": "2025-01-10T23:43:55.380+00:00",
+        //             "updatedAt": "2025-01-10T23:43:55.380+00:00"
+        //         }
+        //     ],
+        //     "projectTopic": "DESKTOP",
+        //     "projectState": true,
+        //     "projectUrl": "https://my-unsplash-frontend-five.vercel.app/",
+        //     "createdAt": "2025-01-12T23:09:53.002+00:00",
+        //     "updatedAt": "2025-01-12T23:09:53.002+00:00"
+        // },
+        // {
+        //     "id": 54,
+        //     "projectName": "Notes App",
+        //     "projectDescription": "Project made with Expo React Native",
+        //     "projectTechnologies": [
+        //         {
+        //             "id": 9,
+        //             "techName": "bx bx-mobile-alt",
+        //             "state": true,
+        //             "createdAt": "2025-01-10T23:44:57.496+00:00",
+        //             "updatedAt": "2025-01-10T23:44:57.496+00:00"
+        //         }
+        //     ],
+        //     "projectTopic": "MOBILE",
+        //     "projectState": true,
+        //     "projectUrl": "https://expo.dev/accounts/jopt05/projects/react-native-notes-app/builds/55f4bc9a-cd7c-424e-85bc-58d3b6716b4d",
+        //     "createdAt": "2025-01-12T23:10:55.296+00:00",
+        //     "updatedAt": "2025-01-13T02:17:29.755+00:00"
+        // },
         {
             "id": 100,
             "projectName": "Mood tracker",
